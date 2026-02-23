9e048374a71251b2979a9dabd1f09e8e1414135c
matheshs806-sudo <matheshs806@gmail.com>
2026-02-23 10:44:51 +0530

    Add files via upload

---
+++ selection sort.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, temp, minIndex;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    // Selection Sort logic
+    for (int i = 0; i < n - 1; i++)
+    {
+        minIndex = i;
+
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+                minIndex = j;
+        }
+
+        // Swap
+        temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+
+    printf("Sorted array:\n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
